4053569fc2a0f445b059797ec29073dbe210db22
남승균 <skyun.nam@gmail.com>
2019-04-18 10:34:00 +0900

    홈: 메인 플로우 개선

--- VigPlan/WebContent/WEB-INF/views/home/home.jsp
+++ VigPlan/WebContent/WEB-INF/views/home/home.jsp
@@ -1,3 +1,4 @@
+<%@page import="com.vigplan.vo.MemberVo"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -10,19 +11,22 @@
  <script type="text/javascript">
  $(document).ready(function() {
 	 $("#regist_btn").click(function() {
-			location.assign('/VigPlan/member'); 
+			location.assign('<%= request.getContextPath() %>/member'); 
 	 });
 	 $("#login_btn").click(function() {
-			location.assign('/VigPlan/member/login'); 
+			location.assign('<%= request.getContextPath() %>/member/login'); 
+	 });
+	 $("#logout_btn").click(function() {
+			location.assign('<%= request.getContextPath() %>/member/logout'); 
 	 });
 	 $("#board_btn").click(function() {
-			location.assign('/VigPlan/board'); 
+			location.assign('<%= request.getContextPath() %>/board'); 
 	 });
 	 $("#group_btn").click(function() {
-			location.assign('/VigPlan/group'); 
+			location.assign('<%= request.getContextPath() %>/group'); 
 	 });
 	 $("#moim_btn").click(function() {
-			location.assign('/VigPlan/moim'); 
+			location.assign('<%= request.getContextPath() %>/moim'); 
 	 });
  });
  
@@ -34,10 +38,20 @@
 <body>
 
 <div class="container">
+<%
+MemberVo authUser = (MemberVo)session.getAttribute("authUser");
+%>
 	<h1>메인페이지</h1>
+<% if (authUser != null) { %>
+	<h3>현재 사용자: <%= authUser.getNickname() %>(<%= authUser.getId() %>)</h3>
+<% } %>
 	<hr>
 	<button id="regist_btn" class="btn">회원가입 하기</button>
+<% if (authUser == null) { %>	
 	<button id="login_btn" class="btn">로그인 하기</button>
+<% } else { %>
+	<button id="logout_btn" class="btn">로그아웃 하기</button>
+<% } %>
 	<button id="board_btn" class="btn">게시판</button>
 	<button id="group_btn" class="btn">그룹</button>
 	<button id="moim_btn" class="btn">모임</button>
--- VigPlan/src/com/vigplan/servlet/member/LoginServlet.java
+++ VigPlan/src/com/vigplan/servlet/member/LoginServlet.java
@@ -13,7 +13,6 @@ import com.vigplan.dao.member.MemberDao;
 import com.vigplan.servlet.BaseServlet;
 import com.vigplan.vo.MemberVo;
 
-
 //	TODO: 서블릿 요청을 만들고(extends BaseServlet 구조)
 //	TODO: 데이터 접속부는 모두 DAO로 추출
 @SuppressWarnings("serial")
@@ -21,23 +20,20 @@ import com.vigplan.vo.MemberVo;
 public class LoginServlet extends BaseServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-			
-			String action = request.getParameter("a");
-			
+
+		String action = request.getParameter("a");
+
 		if (action == null) {
-				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/member/login_form.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/member/login_form.jsp");
 			rd.forward(request, response);
-			} else if ("success".equals(action)){	//	/member/login?a=success
-				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/member/login_success.jsp");
-				rd.forward(request, response);
-			} else if ("fail".equals(action)) {
-				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/member/login_fail.jsp");
-				rd.forward(request, response);
-			}
+		} else if ("fail".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/member/login_fail.jsp");
+			rd.forward(request, response);
+		}
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 1. 요청 파라미터 조회
 		request.setCharacterEncoding("UTF-8");
@@ -45,98 +41,62 @@ public class LoginServlet extends BaseServlet {
 		String password = request.getParameter("pw");
 //		String nickname = request.getParameter("nickname");
 //		String email = request.getParameter("email");
-		
+
 		/*
-		ResultSet rs = null;
-		String sql = "select id, password, nickname, email, rDate from member where id=?";
-		
-		HttpSession session = request.getSession();
-		*/
+		 * ResultSet rs = null; String sql =
+		 * "select id, password, nickname, email, rDate from member where id=?";
+		 * 
+		 * HttpSession session = request.getSession();
+		 */
 		MemberDao dao = new MemberDao(dbuser, dbpass);
-		
+
 		MemberVo vo = dao.getMember(id, password);
 		System.out.println("LOGIN: VO = " + vo);
 		if (vo != null) {
 			HttpSession session = request.getSession();
 			session.setAttribute("authUser", vo);
-			
-			//	TODO: 로그인 성공 페이지로 리다이렉트
-			response.sendRedirect(request.getContextPath() + "/member/login?a=success");
+
+			// Home으로 Redirect
+			response.sendRedirect(request.getContextPath());
 		} else {
 //			TODO: 실패 -> login 페이지로 리다이렉트
 			response.sendRedirect(request.getContextPath() + "/member/login?a=fail");
 		}
-		
+
 //		session.setAttribute("id", "pw");
 
 		/*
-		// Business Login 처리
-		ServletContext sct = getServletContext();
-		String url = sct.getInitParameter("url");
-		String user = sct.getInitParameter("user");
-		String DBPwd = sct.getInitParameter("password");
-		
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		
-		String sql = "select id, password, nickName, eMail, rDate from member where id=?";
-		
-		String resURL = null;
-		
-		try {
-			conn = DriverManager.getConnection(url, user, DBPwd);
-			
-			psmt = conn.prepareStatement(sql);
-			
-			psmt.setString(1, id);
-			
-			rs = psmt.executeQuery();
-			// 아이디가 있으면
-			if(rs.next()) { // 아이디가 있는 경우 실행
-				// 패스워드 비교
-				if(password.equals(rs.getString(2))) { // ID와 PW가 맞는 경우
-					HttpSession session = request.getSession();
-					MemberVO vo = new MemberVO(id, password, rs.getString(3), 
-							rs.getString(4), rs.getString(5));
-					
-					session.setAttribute("login_info", vo);
-					response.sendRedirect("/com/vigplan/res/login_success.jsp");
-				} else { // 패스워드가 틀린 경우
-					request.setAttribute("error_message","패스워드가 틀렸습니다.");
-					resURL = "/login_form.jsp";
-				}
-			} else { // id가 없는 경우
-				request.setAttribute("error_message", id + "는 없는 ID 입니다.");
-				resURL = "login_form.jsp";
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			request.setAttribute("error_message", e.getMessage());
-			resURL = "/res/error.jsp";
-		} finally {
-		}
-		if(resURL != null){
-			RequestDispatcher rdp = request.getRequestDispatcher(resURL);
-			rdp.forward(request, response);
-		}
-		*/
+		 * // Business Login 처리 ServletContext sct = getServletContext(); String url =
+		 * sct.getInitParameter("url"); String user = sct.getInitParameter("user");
+		 * String DBPwd = sct.getInitParameter("password");
+		 * 
+		 * Connection conn = null; PreparedStatement psmt = null; ResultSet rs = null;
+		 * 
+		 * String sql =
+		 * "select id, password, nickName, eMail, rDate from member where id=?";
+		 * 
+		 * String resURL = null;
+		 * 
+		 * try { conn = DriverManager.getConnection(url, user, DBPwd);
+		 * 
+		 * psmt = conn.prepareStatement(sql);
+		 * 
+		 * psmt.setString(1, id);
+		 * 
+		 * rs = psmt.executeQuery(); // 아이디가 있으면 if(rs.next()) { // 아이디가 있는 경우 실행 //
+		 * 패스워드 비교 if(password.equals(rs.getString(2))) { // ID와 PW가 맞는 경우 HttpSession
+		 * session = request.getSession(); MemberVO vo = new MemberVO(id, password,
+		 * rs.getString(3), rs.getString(4), rs.getString(5));
+		 * 
+		 * session.setAttribute("login_info", vo);
+		 * response.sendRedirect("/com/vigplan/res/login_success.jsp"); } else { //
+		 * 패스워드가 틀린 경우 request.setAttribute("error_message","패스워드가 틀렸습니다."); resURL =
+		 * "/login_form.jsp"; } } else { // id가 없는 경우
+		 * request.setAttribute("error_message", id + "는 없는 ID 입니다."); resURL =
+		 * "login_form.jsp"; } } catch (SQLException e) { e.printStackTrace();
+		 * request.setAttribute("error_message", e.getMessage()); resURL =
+		 * "/res/error.jsp"; } finally { } if(resURL != null){ RequestDispatcher rdp =
+		 * request.getRequestDispatcher(resURL); rdp.forward(request, response); }
+		 */
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
